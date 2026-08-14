ee358e8c00e97bd1d7ee58d79fc1e12d39964b28
Sina Hosseinizad <24656395+wo-von@users.noreply.github.com>
2026-08-14 19:27:43 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ int main() {
     std::cout << "$ ";
     std::string command;
     std::getline(std::cin, command);
+    if (command == "exit")
+      break;
     std::cout << command << ": command not found" << std::endl;
   }
 }
